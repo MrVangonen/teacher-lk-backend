2d66f57af61883a7ae214efca30ee74f6032e847
Daniil Vangonen <mr.vangonen@gmail.com>
2020-06-23 17:04:49 +0300

    i

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const cool = require('cool-ascii-faces');
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
@@ -9,14 +8,5 @@ app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
+  .get('/', (req, res) => res.send('Hi'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-// app.get('/get', function (req, res) {
-//   res.send('Hello World!');
-// });
-
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!');
-// });
